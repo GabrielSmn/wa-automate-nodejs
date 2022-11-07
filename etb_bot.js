432acd10262698ec0e12fd7233f158376946af6d
GabrielSmn <gabriel.simeone@gmail.com>
2022-11-06 22:10:50 -0300

    fix(etb_bot)filtro ajustado

--- etb_bot.js
+++ etb_bot.js
@@ -15,63 +15,61 @@ wa.create({
   qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
 }).then((client) => start(client));
 
-function safeGuard(number){
-    let corrigir = number.replace("@c.us","")
-    console.log(corrigir.length)
-    if(corrigir.length<13){
-        console.log("limpando : ",corrigir)
-        const nXnf = number.slice(0,4)+'9'+number.slice(4,)
-        return nXnf
-    }
-    console.log("tava limpo já", corrigir)
-    return corrigir
+function safeGuard(number) {
+  let corrigir = number.replace('@c.us', '');
+  console.log(corrigir.length);
+  if (corrigir.length < 13) {
+    console.log('limpando : ', corrigir);
+    const nXnf = number.slice(0, 4) + '9' + number.slice(4);
+    return nXnf;
+  }
+  console.log('tava limpo já', corrigir);
+  return corrigir;
 }
-
-
 
 function start(client) {
   client.onMessage(async (message) => {
-
     var data = JSON.stringify({
-        phone:  safeGuard(message.from),
-        token: 'AAAAC3NzaC1lZDI1NTE5AAAAIADyn0X4gYpS+GAlPXG4tjXBGduGXRD50KVIQu3hmbjG',
-      });
-      
-      var config = {
-        method: 'post',
-        url: 'https://etb-api-prod.herokuapp.com/get-user-by-phone',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        data: data,
-      };
-     
+      phone: safeGuard(message.from),
+      token:
+        'AAAAC3NzaC1lZDI1NTE5AAAAIADyn0X4gYpS+GAlPXG4tjXBGduGXRD50KVIQu3hmbjG',
+    });
+
+    var config = {
+      method: 'post',
+      url: 'https://etb-api-prod.herokuapp.com/get-user-by-phone',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+
     axios(config)
       .then(async function (response) {
-        console.log("response",response.data)
+        console.log('response', response.data);
         if (response.data.name) {
           await client.sendText(
             message.from,
             `Boa ${response.data.name}, inscrição confirmada!`
           );
 
-          config.url = 'https://etb-api-prod.herokuapp.com/get-user-by-phone'
+          config.url = 'https://etb-api-prod.herokuapp.com/get-user-by-phone';
           axios(config)
-          .then(function (response) {
-            console.log(JSON.stringify(response.data.phone));
-          })
-          .catch(async function (error) {
-            await client.sendText(
-                safeGuard(message.from,
-                `Sai pra lá, aqui você não se inscreveu não!`)
+            .then(function (response) {
+              console.log(JSON.stringify(response.data.phone));
+            })
+            .catch(async function (error) {
+              await client.sendText(
+                  message.from,
+                  `Sai pra lá, tem nada seu aqui não!`
               );
-            console.log("Deu erro essa bagaça :",error);
-          });
-
-
-
+              console.log('Deu erro essa bagaça :', error);
+            });
         } else {
-            await client.sendText(message.from,'Sai pra lá, vc não tem cadastro aqui não.')
+          await client.sendText(
+            message.from,
+            'Sai pra lá, vc não tem cadastro aqui não.'
+          );
           console.log(
             'Não é quem eu estou esperando, response :',
             JSON.stringify(response.data)
